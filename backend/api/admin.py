a069ef886398621a652122bc542fb16724bd7f57
Scorcherfjk <franjavikale@gmail.com>
2021-04-16 09:24:45 -0500

    registering custom config to admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from django.utils.translation import gettext, gettext_lazy as _
+from .models import User, Headquarters
 
 # Register your models here.
-admin.site.register(User, BaseUserAdmin)
+
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': (
+            'first_name', 'last_name', 'email', 'phone_number', 'date_of_birth', 'headquarters')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('last_login', 'date_joined', 'email')
+
+
+admin.site.register(Headquarters)
