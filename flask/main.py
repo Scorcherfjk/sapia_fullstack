e299c3678c83530612d54aade767e9934d3b033c
Scorcherfjk <franjavikale@gmail.com>
2021-04-16 15:50:28 -0500

    integration from libraries  and connections

--- flask/main.py
+++ flask/main.py
@@ -1,14 +1,35 @@
+# Python
+from datetime import timedelta
 from os import urandom
-from flask import Flask, jsonify
+
+# Flask
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+from flask_jwt import JWT, jwt_required, current_identity
+
+# Internal
+from sapia.routes import public, protected
+from sapia.config.jwt import connect
 
 app = Flask(__name__)
 app.secret_key = urandom(24)
 
-@app.route('/')
-def index():
-	return jsonify({
-    'hola': "mundo"
-  })
+# Bcrypt
+bcrypt = Bcrypt(app)
+app.config["BCRYPT"] = bcrypt
+
+# Mongo
+mongodb_client = PyMongo(app, uri="mongodb://root:example@localhost:27017/sapia?authSource=admin")
+db = mongodb_client.db
+app.config["DB"] = db
+
+# Instance JWT
+jwt = connect(app)
+
+# Routes
+public.PublicRoutes(app).routes()
+protected.ProtectedRoutes(app).routes()
 
 if __name__ == '__main__':
-	app.run(port=5010, debug=True)
+	app.run(port=5000, debug=True)
