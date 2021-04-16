1b010507bfcdab9106b424ec31c021f309c5a9b3
Scorcherfjk <franjavikale@gmail.com>
2021-04-16 09:24:20 -0500

    adding fields to User model and Headquarters

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,6 +5,22 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
+
+
+class Headquarters(models.Model):
+    name = models.CharField(max_length=50, null=True,
+                            blank=True, verbose_name="Nombre")
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Sede"
+        verbose_name_plural = "Sedes"
+
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password, **extra_fields):
@@ -36,6 +52,12 @@ class User(AbstractUser):
         editable=False
     )
     email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(
+        blank=True, null=True, verbose_name="Fecha de nacimiento")
+    phone_number = models.IntegerField(
+        blank=True, null=True, verbose_name="Numero de telefono")
+    headquarters = models.ForeignKey(
+        Headquarters, blank=True, null=True, on_delete=models.DO_NOTHING, verbose_name="Sede")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
