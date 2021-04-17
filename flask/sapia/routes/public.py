e1c14f06da5d96fe2915c519c54feceb7217eb30
Scorcherfjk <franjavikale@gmail.com>
2021-04-16 19:54:37 -0500

    completing endpoint 'create'

--- flask/sapia/routes/public.py
+++ flask/sapia/routes/public.py
@@ -1,5 +1,6 @@
 import json
 import hashlib
+from datetime import datetime
 from flask import jsonify, request, Response
 
 from sapia.models.user import User
@@ -19,19 +20,21 @@ class PublicRoutes():
                 data = request.json
 
                 user = User(
-                    id = None,
-                    name = data['name'],
-                    lastname = data['lastname'], 
-                    phone_number = data['phone_number'], 
-                    date_of_birth = data['date_of_birth'], 
-                    email = data['email'], 
-                    password = self.bcrypt.generate_password_hash(data['password'])
-                    )
+                    id=None,
+                    name=data['name'],
+                    lastname=data['lastname'],
+                    phone_number=data['phone_number'],
+                    date_of_birth=datetime.strptime(data['date_of_birth'], '%Y-%m-%d'),
+                    email=data['email'],
+                    password=self.bcrypt.generate_password_hash(
+                        data['password']),
+                    headquarter=data.get('headquarter', None),
+                    program=data.get('program', None),
+                )
                 obj = user.toDict()
 
                 self.db.user.insert_one(obj)
 
                 return jsonify({"message": "success"})
             except Exception as e:
-                print(e)
-                return Response(json.dumps({"message": "error"}), status=400, content_type="application/json")
+                return Response(json.dumps({"message": str(e)}), status=400, content_type="application/json")
